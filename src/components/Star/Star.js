be15aad8a8ceba532c69eee9e6ae7112b1681494
petecoj <petecojohns@gmail.com>
2018-07-25 10:19:13 -0500

    curried this bad boy

--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -1,55 +1,78 @@
 import React, { Component } from 'react';
 
 class Star extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             star: {
                 name: '',
-                famousFor: ''
+                famousFor: '',
+                popularity: ''
             }
         }
     }
-    handleName = (event) => {
-        console.log(event.target.value);
 
+    handleChangeFor = (propertyName) => (event) => {
         this.setState({
             star: {
                 ...this.state.star,
-                name: event.target.value
+                [propertyName]: event.target.value
             }
         })
     }
-    handleFamousFor = (event) => {
-        console.log(event.target.value);
+    // handleName = (event) => {
+    //     console.log(event.target.value);
 
-        this.setState({
-            star: {
-                ...this.state.star,
-                famousFor: event.target.value
-                
-            }
-        })
-    }
+    //     this.setState({
+    //         star: {
+    //             ...this.state.star,
+    //             name: event.target.value
+    //         }
+    //     })
+    // }
+    // handleFamousFor = (event) => {
+    //     console.log(event.target.value);
+
+    //     this.setState({
+    //         star: {
+    //             ...this.state.star,
+    //             famousFor: event.target.value
+
+    //         }
+    //     })
+    // }
     handleClick = (event) => {
-       console.log(this.state.star)
-
+        console.log(this.state.star)
+        event.preventDefault();
+        this.setState({
+            star: {
+                name: '',
+                famousFor: '',
+                popularity: ''
+            }
+        })
+     
     }
 
 
 
 
-    render(){
+    render() {
         return (
             <div>
-                <input type="text" onChange={this.handleName}/>
-                <input type="text" onChange={this.handleFamousFor}/>
-                <button onClick={this.handleClick}>POST</button>
-                <div>
-                    <p>{this.state.star.name}</p>
-                    <p>is famous for</p>
-                    <p>{this.state.star.famousFor}</p>
-                </div>
+                <form onSubmit={this.handleClick}>
+                    <input value={this.state.star.name} type="text" onChange={this.handleChangeFor('name')} />
+                    <input value={this.state.star.famousFor} type="text" onChange={this.handleChangeFor('famousFor')} />
+                    <input value={this.state.star.popularity} type='number' onChange={this.handleChangeFor('popularity')} />
+                    <input type="submit" value="Submit"/>
+                    <div>
+                        <p>{this.state.star.name}</p>
+                        <p>is famous for</p>
+                        <p>{this.state.star.famousFor}</p>
+                        <p>and popularity score is</p>
+                        <p>{this.state.star.popularity}</p>
+                    </div>
+                </form>
             </div>
 
         )
